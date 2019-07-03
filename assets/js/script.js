b7ed3e66ac2cdb36a993973d33110ca62c24577b
Jase Thomson <jasethomson31@gmail.com>
2019-07-03 09:20:44 -0700

    Memory Match - Flip One card - Jase Thomson

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+$(document).ready(intializeApp);
 
+function intializeApp(){
+  var cardToClick = $('.lfzCard');
+  cardToClick.click(flipCard);
+
+  function flipCard(){
+  $(this).css({"display": "none"});
+  }
+}
