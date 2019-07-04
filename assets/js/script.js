184096e2c7717cbcaddff6d63dabe3c0bb105e8d
Jase Thomson <jasethomson31@gmail.com>
2019-07-03 20:46:51 -0700

    skeleton-v.02

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+$(document).ready(intializeApp);
 
+function intializeApp(){
+  $(".lfzCard").on("click", handleCardClick);
+}
+function handleCardClick(event){
+  var clickCurrentTarget = $(event.currentTarget);
+  clickCurrentTarget.addClass('hidden');
+}
