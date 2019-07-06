9346c245cf7565adc2d3069bd1f61adf05eb8923
Jase Thomson <jasethomson31@gmail.com>
2019-07-06 15:54:28 -0700

    working draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,12 @@ var secondCardClicked = null;
 var firstCardUrl = null;
 var secondCardUrl = null;
 var matches = null;
-
+var max_matches = 2;
 function intializeApp(){
   $(".lfzCard").on("click", handleCardClick);
 }
 function handleCardClick(event){
+
   var clickCurrentTarget = $(event.currentTarget);
   clickCurrentTarget.addClass('hidden');
   if (!firstCardClicked){
@@ -17,26 +18,44 @@ function handleCardClick(event){
   } else if(!secondCardClicked){
     secondCardClicked = clickCurrentTarget;
     secondCardUrl = $(secondCardClicked).siblings().css("background-image");
+    $(".lfzCard").off("click", handleCardClick);
     if (firstCardUrl === secondCardUrl){
+      firstCardClicked.siblings().hide(2000);
+      secondCardClicked.siblings().hide(2000);
       console.log("cards match");
       matches++;
-      flipCardsBack();
+      firstCardClicked = null;
+      secondCardClicked = null;
+      $(".lfzCard").on("click", handleCardClick);
+      if(max_matches === matches){
+        console.log('you won!!!');
+        createModal();
+      }
     } else {
       console.log("cards don't match");
       flipCardsBack();
     }
   }
-  console.log('firstCardClicked: ',firstCardClicked);
-  console.log('secondCardClicked: ',secondCardClicked);
-  console.log('firstCardUrl: ',firstCardUrl);
-  console.log('secondCardUrl: ',secondCardUrl);
-  console.log("matches: ", matches);
 }
 function flipCardsBack(){
   setTimeout(function () {
+    $(".lfzCard").on("click", handleCardClick);
     firstCardClicked.removeClass('hidden');
     secondCardClicked.removeClass('hidden');
     firstCardClicked = null;
     secondCardClicked = null;
   }, 1500);
+}
+function createWinningDiv(){
+  var winningDiv = $('<div>');
+  winningDiv.text("You won!!!")
+  var playAgain = $('<button>');
+  playAgain.text('Play again?');
+  $("button").on("click", resetGame);
+  $('body').append(winningDiv);
+
+}
+function resetGame(event){
+  var buttonPressed = $(event.currentTarget);
+  $('div').removeClass('hidden');
 }
