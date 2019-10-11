7067a6af7ef597316755533724d74d88f8e9fd2d
jasethomson <jasethomson31@gmail.com>
2019-10-11 07:50:01 -0700

    Added required files to highscore.php, sent JSON header, set exception handler, added query for scores, added error checking for query, query rows check, and created associative array to be created from query result, printed output.

---
+++ server/public/api/highScore.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'functions.php';
+set_exception_handler('error_handler');
+startup();
+require_once 'db_connection.php';
+
+$scoreQuery = "SELECT id, name, attempts, accuracy
+  FROM `highScore`";
+
+$scoreResult = mysqli_query($conn, $scoreQuery);
+
+if(!$scoreResult){
+  throw new Exception("Connect Failed: " . mysqli_error($conn));
+}
+
+$row_cnt = mysqli_num_rows($scoreResult);
+if( $row_cnt === 0) {
+  throw new Exception("No Rows returned");
+}
+
+$output = [];
+
+while($row = mysqli_fetch_assoc($scoreResult)) {
+  $output[] = $row;
+}
+
+$json_output = json_encode($output);
+print($json_output);
+
+?>
